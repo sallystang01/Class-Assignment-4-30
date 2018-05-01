13696774d633d109210675be76e956e388be20a2
sallystang01 <hunterduerr@gmail.com>
2018-04-30 20:44:29 -0400

    Upload

---
+++ Assignment/Assignment/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNames = new System.Windows.Forms.ListBox();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbNames
+            // 
+            this.lbNames.FormattingEnabled = true;
+            this.lbNames.Location = new System.Drawing.Point(12, 64);
+            this.lbNames.Name = "lbNames";
+            this.lbNames.Size = new System.Drawing.Size(231, 134);
+            this.lbNames.TabIndex = 0;
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(168, 35);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(75, 23);
+            this.btnPop.TabIndex = 1;
+            this.btnPop.Text = "Search";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(168, 263);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "New Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter a Name:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "5000 results found in x seconds";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 298);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.lbNames);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbNames;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Assignment/Assignment/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+          
+
+            try
+            {
+                // Declaration of Variables
+                // Contains the path for the stream readers and writers
+                string txtPath1 = "Names.csv";
+                string txtPath2 = "NewNames.csv";
+                // Stream Reader to read the text files
+                StreamReader sReader = File.OpenText(txtPath1);
+                // Stream Writer to write the text files
+                StreamWriter sWriter = new StreamWriter(txtPath2);
+                
+               
+
+                
+                
+                
+                    int i;
+                    i = 0;
+                    string[] arNames = new string[5000];
+
+                    for (i = 0; i < 5000; i++)
+                    {
+                        arNames[i] = sReader.ReadLine();
+                    Array.Sort(arNames);
+                        lbNames.Items.Add(arNames[i]);
+                        
+                    }
+
+
+                foreach (string name in lbNames.Items)
+                {
+                    sWriter.WriteLine(name.ToString());
+                }
+
+                
+                }
+            
+            catch (Exception)
+            {
+
+
+                throw;
+            }
+
+
+
+
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
