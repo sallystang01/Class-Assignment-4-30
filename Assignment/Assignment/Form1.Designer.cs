af86e28c2c50e473717029e96b590177a60cc97f
sallystang01 <hunterduerr@gmail.com>
2018-05-01 21:36:51 -0400

    Done for now

--- Assignment/Assignment/Form1.Designer.cs
+++ Assignment/Assignment/Form1.Designer.cs
@@ -32,32 +32,38 @@
             this.btnPop = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbNames
             // 
             this.lbNames.FormattingEnabled = true;
-            this.lbNames.Location = new System.Drawing.Point(12, 64);
+            this.lbNames.Location = new System.Drawing.Point(12, 83);
             this.lbNames.Name = "lbNames";
             this.lbNames.Size = new System.Drawing.Size(231, 134);
             this.lbNames.TabIndex = 0;
             // 
             // btnPop
             // 
-            this.btnPop.Location = new System.Drawing.Point(168, 35);
+            this.btnPop.Location = new System.Drawing.Point(118, 55);
             this.btnPop.Name = "btnPop";
             this.btnPop.Size = new System.Drawing.Size(75, 23);
             this.btnPop.TabIndex = 1;
             this.btnPop.Text = "Search";
             this.btnPop.UseVisualStyleBackColor = true;
-            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(168, 263);
+            this.btnClose.Location = new System.Drawing.Point(98, 311);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 23);
             this.btnClose.TabIndex = 2;
@@ -67,53 +73,98 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(168, 234);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "New Search";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Location = new System.Drawing.Point(13, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Enter a Name:";
             // 
-            // label2
+            // groupBox1
             // 
-            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 201);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(230, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "5000 results found in x seconds";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.groupBox1.Controls.Add(this.lblTime);
+            this.groupBox1.Location = new System.Drawing.Point(12, 223);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 82);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(9, 13);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(212, 66);
+            this.lblTime.TabIndex = 8;
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(254, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFileToolStripMenuItem,
+            this.sortResultsToolStripMenuItem,
+            this.exportFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // loadFileToolStripMenuItem
+            // 
+            this.loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            this.loadFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadFileToolStripMenuItem.Text = "&Load File";
+            this.loadFileToolStripMenuItem.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
+            // 
+            // sortResultsToolStripMenuItem
+            // 
+            this.sortResultsToolStripMenuItem.Name = "sortResultsToolStripMenuItem";
+            this.sortResultsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sortResultsToolStripMenuItem.Text = "&Sort Results";
+            this.sortResultsToolStripMenuItem.Click += new System.EventHandler(this.sortResultsToolStripMenuItem_Click);
+            // 
+            // exportFileToolStripMenuItem
+            // 
+            this.exportFileToolStripMenuItem.Name = "exportFileToolStripMenuItem";
+            this.exportFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportFileToolStripMenuItem.Text = "E&xport File";
+            this.exportFileToolStripMenuItem.Click += new System.EventHandler(this.exportFileToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(254, 298);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(254, 348);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnPop);
             this.Controls.Add(this.lbNames);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,9 +176,14 @@
         private System.Windows.Forms.Button btnPop;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortResultsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportFileToolStripMenuItem;
     }
 }
 
--- Assignment/Assignment/Form1.cs
+++ Assignment/Assignment/Form1.cs
@@ -16,48 +16,63 @@ namespace Assignment
         {
             InitializeComponent();
         }
+        const string txtPath1 = "Names.csv";
+        // Variables
+        static string[] arNames = new string[5000];
+        StreamReader sReader = File.OpenText(txtPath1);
+        int i;
+        DateTime start = DateTime.Now;
+        DateTime Finish;
+        TimeSpan Time;
 
-        private void btnPop_Click(object sender, EventArgs e)
+
+
+        private void btnClose_Click(object sender, EventArgs e)
         {
-          
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void loadFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
 
             try
             {
-                // Declaration of Variables
-                // Contains the path for the stream readers and writers
-                string txtPath1 = "Names.csv";
-                string txtPath2 = "NewNames.csv";
-                // Stream Reader to read the text files
-                StreamReader sReader = File.OpenText(txtPath1);
-                // Stream Writer to write the text files
-                StreamWriter sWriter = new StreamWriter(txtPath2);
+
                 
+                // Declaration of Variables
+                // Contains the path for the stream reader
+               
+                
+                // Stream Reader to read the text files
                
 
-                
-                
-                
-                    int i;
-                    i = 0;
-                    string[] arNames = new string[5000];
 
-                    for (i = 0; i < 5000; i++)
-                    {
-                        arNames[i] = sReader.ReadLine();
-                    
-                        lbNames.Items.Add(arNames[i]);
-                        
-                    }
+                // Variables
 
+                
+                i = 0;
 
-                foreach (string name in lbNames.Items)
+                // Increment my i variable
+                for (i = 0; i < 5000; i++)
                 {
-                    sWriter.WriteLine(name.ToString());
+                    // Adds each line that my stream reader reads
+                    // into the array instance of i
+                    arNames[i] = sReader.ReadLine();
+                    // adds the instance of i into the list box
+                    lbNames.Items.Add(arNames[i]);
+                    
                 }
 
-                
-                }
-            
+                Finish = DateTime.Now;
+                Time = Finish - start;
+                lblTime.Text = lbNames.Items.Count.ToString() + " " + "Results Loaded in" + " " + (Time.ToString());
+            }
+
             catch (Exception)
             {
 
@@ -68,12 +83,83 @@ namespace Assignment
 
 
 
-
         }
 
-        private void btnClose_Click(object sender, EventArgs e)
+        private void exportFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            // Contains the path for the stream writer
+            string txtPath2 = "NewNames.csv";
+            // Stream Writer to write the text files
+            StreamWriter sWriter = new StreamWriter(txtPath2);
+            // Writes the sorted values into the new file
+            foreach (string name in lbNames.Items)
+            {
+                sWriter.WriteLine(name.ToString());
+            }
+
         }
+
+       public void SelectionSort(string[] arNames)
+        {
+            int minIndex;
+            string minValue;
+            int startScan = 0;
+
+            for (startScan = 0; startScan < arNames.Length - 1; startScan++)
+            {
+                minIndex = startScan;
+                minValue = arNames[startScan];
+
+
+
+
+                for (int index = startScan + 1; index < arNames.Length; index++)
+                {
+                    if (string.Compare(minValue, arNames[index], true) == 1)
+                    {
+                        minValue = arNames[index];
+                        minIndex = index;
+                    }
+                }
+                Swap(ref arNames[minIndex], ref arNames[startScan]);
+            }
+
+
+
+        }
+
+       private void Swap(ref string a, ref string b)
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+
+        }
+
+       
+
+        private void sortResultsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            while (i < arNames.Length && !sReader.EndOfStream)
+            {
+                arNames[i] = sReader.ReadLine();
+                i++;
+            }
+            lbNames.Items.Clear();
+            SelectionSort(arNames);
+
+            foreach (string name in arNames)
+            {
+                lbNames.Items.Add(name);
+            }
+
+            Finish = DateTime.Now;
+            Time = Finish - start;
+            
+            lblTime.Text = lbNames.Items.Count.ToString() + " " + "Results Sorted in" + " " + (Time.TotalSeconds.ToString()) + " " + "seconds";
+        }
+
     }
 }
